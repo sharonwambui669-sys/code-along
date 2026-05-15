222426ef79882ab900c8488b2e9d7e591429735d
Sharon Wambui <sharonwambui669@gmail.com>
2026-05-15 04:54:28 +0300

    feat:creation of constructor function section

---
+++ script.js
@@ -0,0 +1,13 @@
+// ===============================
+// TO-DO LIST APPLICATION
+// ===============================
+
+// Constructor Function
+
+function Task(taskName, taskDate) {
+
+  this.taskName = taskName;
+  this.taskDate = taskDate;
+  this.completed = false;
+
+}
