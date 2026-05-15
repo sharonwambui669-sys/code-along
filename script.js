6612bfcfc406dcc3b0abb6cbda83167e987d944a
Sharon Wambui <sharonwambui669@gmail.com>
2026-05-15 05:04:01 +0300

    chore:get user input form

--- script.js
+++ script.js
@@ -36,3 +36,24 @@ const taskList = document.getElementById("taskList");
 // ADD TASK FUNCTIONALITY
 // ===============================
 
+// Selecting HTML Elements
+
+const taskForm = document.getElementById("taskForm");
+
+const taskList = document.getElementById("taskList");
+
+// ===============================
+// ADD TASK FUNCTIONALITY
+// ===============================
+
+taskForm.addEventListener("submit", function(event) {
+
+  // Prevent Page Refresh
+
+  event.preventDefault();
+
+  // Get User Input
+
+  const taskName = document.getElementById("taskName").value;
+
+  const taskDate = document.getElementById("taskDate").value;
